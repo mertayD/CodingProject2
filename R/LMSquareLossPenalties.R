b2a0a1e03685900ea18f9d50c4fc43d48c78fab9
mertayd <md2289@nau.edu>
2019-02-21 00:33:56 -0700

    Penalties.R Done

---
+++ R/LMSquareLossPenalties.R
@@ -0,0 +1,23 @@
+LMSquareLossL2Penalties <- function(
+  X.mat, 
+  y.vec, 
+  penalty.vec 
+){
+  mean.mat <- matrix(rep(colMeans(X.mat)),nrow = ncol(X.mat), ncol = 1)
+  zero_mean <- sweep(X.mat,2, as.vector(mean.mat),"-")
+  squared_zero_mean <- (zero_mean)^2
+  squared.means = colMeans(squared_zero_mean)
+  S.vec <- sqrt((squared.means))
+  S.diagonal.mat <- diag(S.vec^-1, nrow = ncol(X.mat), ncol = ncol(X.mat))
+  X.scaled <- sweep(zero_mean,2,S.vec,"/")
+  
+  i <- 0
+  for(penalty in penalty.vec)
+  {
+    i <- i + 1
+    w.mat[,i] <- LMSquareLossL2(X.scaled,y.vec, penalty)
+  }
+  
+  W.mat <- t(w.mat) %*% S.diagonal.mat
+  return(W.mat)
+}
